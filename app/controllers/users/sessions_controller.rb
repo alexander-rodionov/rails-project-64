e91af781ca8d554d2ad0bea3966450536cc24d76
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-03-26 23:19:35 +0000

    htg19

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,7 +18,6 @@ class Users::SessionsController < Devise::SessionsController
     super do |user|
       render json: { user: user.as_json(only: %i[id email]) } and return if request.format.json?
       redirect_to root_path and return
-      end
     end
   end
 
