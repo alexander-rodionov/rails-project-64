53ee3692005e247bb91dd550fcd62b13cff5f07f
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-03-26 18:37:58 +0000

    htg5

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,24 +1,28 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
+  before_action :get_post
+
+  def get_post
+    @post = Post.find(params.require(:post_id))
+  end
+
   def toggle
-    post_id = params.require(:post_id)
-    post = Post.find(post_id)
-    if post.post_like.where(user_id: current_user.id).pluck(:id).empty?
-      post.post_like.create(post_id: post_id, user_id: current_user.id)
+    if @post.post_like.where(user: current_user).pluck(:id).empty?
+      @post.post_like.create(post: @post, user: current_user)
     else
-      post.post_like.where(user_id: current_user.id).delete_all
+      @post.post_like.where(user: current_user).delete_all
     end
-    redirect_to post_path(post_id)
+    redirect_to post_path(@post)
   end
 
   def create
     pp params
-    redirect_to post_path(post_id)
+    redirect_to post_path(@post)
   end
   
   def destroy
     pp params
-    redirect_to post_path(post_id)
+    redirect_to post_path(@post)
   end
 
   def params_permitted
