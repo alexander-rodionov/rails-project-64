3c7f84ea4e608e354cdb405f8bffdf9c1bedae7c
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-03-26 14:07:57 +0000

    pretest1

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ group :development, :test do
   gem "sqlite3", ">= 1.4"
 
   gem "byebug"
+  gem "brakeman"
 end
 
 group :development do
--- test/scenarios/not_logged_scenarios.rb
+++ test/scenarios/not_logged_scenarios.rb
@@ -4,8 +4,30 @@ require "test_helper"
 
 class PostCommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    assert User.size>0
+    assert User.exists?(email:'johndoe@mail.cc')
+    assert Category.size>0
+    assert Post.size>0
+    assert PostComment.size>0
   end
 
-  test "should comments#create" do
+  test "home page" do
+
   end
+
+  test "posts listed" do
+    
+  end
+
+
+  test "login possible" do
+    
+  end
+
+  test "registration possible" do
+    
+  end
+
+
+
 end
