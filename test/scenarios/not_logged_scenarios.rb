5dbc29cb018473b2295631a3583f5b041b12403d
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-03-26 14:09:26 +0000

    pretest2

--- test/scenarios/not_logged_scenarios.rb
+++ test/scenarios/not_logged_scenarios.rb
@@ -5,29 +5,22 @@ require "test_helper"
 class PostCommentsControllerTest < ActionDispatch::IntegrationTest
   setup do
     assert User.size>0
-    assert User.exists?(email:'johndoe@mail.cc')
+    assert User.exists?(email: "johndoe@mail.cc")
     assert Category.size>0
     assert Post.size>0
     assert PostComment.size>0
   end
 
   test "home page" do
-
   end
 
   test "posts listed" do
-    
   end
 
 
   test "login possible" do
-    
   end
 
   test "registration possible" do
-    
   end
-
-
-
 end
