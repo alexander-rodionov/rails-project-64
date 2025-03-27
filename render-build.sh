c0bd2a82babce0d122cd66e4a67201b20e8dd04f
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-03-27 21:46:41 +0000

    fix render deploy 2

--- render-build.sh
+++ render-build.sh
@@ -3,11 +3,14 @@
 set -o errexit
 
 if [[ "$RENDER" == "true" ]]; then
-  # Terminate all connections first
-  psql $DATABASE_URL -c "SELECT pg_terminate_backend(pg_stat_activity.pid) 
-                         FROM pg_stat_activity 
-                         WHERE pg_stat_activity.datname = current_database() 
-                         AND pid <> pg_backend_pid();"
+  DB_URL="postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
+
+  # Safely reset the database (with connection handling)
+  psql "${DB_URL}" -c "SELECT pg_terminate_backend(pg_stat_activity.pid) 
+                        FROM pg_stat_activity 
+                        WHERE pg_stat_activity.datname = '${DB_DATABASE}' 
+                        AND pid <> pg_backend_pid();" || true
+
 fi
 
 bundle install
