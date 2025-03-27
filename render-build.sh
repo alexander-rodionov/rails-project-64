7a5963bead18e7ced4aa14220063df3f33928e19
alexander-rodionov <alexander.a.rodionov@gmail.com>
2025-03-27 21:41:06 +0000

    fix render deploy

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,14 @@
 # exit on error
 set -o errexit
 
+if [[ "$RENDER" == "true" ]]; then
+  # Terminate all connections first
+  psql $DATABASE_URL -c "SELECT pg_terminate_backend(pg_stat_activity.pid) 
+                         FROM pg_stat_activity 
+                         WHERE pg_stat_activity.datname = current_database() 
+                         AND pid <> pg_backend_pid();"
+fi
+
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
